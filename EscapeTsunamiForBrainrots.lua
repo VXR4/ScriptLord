eca216a94a3088697062993c16a4bd31ac507514
VXR4 <itzknoxhunter@gmail.com>
2026-01-20 21:30:47 +0700

    Update EscapeTsunamiForBrainrots.lua

--- EscapeTsunamiForBrainrots.lua
+++ EscapeTsunamiForBrainrots.lua
@@ -1,1 +1,2 @@
-game:GetService("Players").LocalPlayer:Kick("Script under maintenance. Back on Jan 14, 2026.")
+-- simple obfuscator, not security
+for _fghijklfghijklmnfghijklmnopqrstuvwxyzABCDEstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345 = 1,10 do warn((function() local k1,k2=110,24; local t={7,13,34,63,36,42,120,34,43,57,46,103,101,31}; local r={}; for i=1,#t do local v = bit32.bxor(t[i], ((k2 + i*3) % 256)); v = bit32.bxor(v, ((k1 + i) % 256)); r[i] = string.char(v); end; return table.concat(r) end)()) end
